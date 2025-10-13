7484e543b7e08685a41ceae49b93ada2befd8442
Luis <lugubagi@gmail.com>
2025-10-13 17:15:45 -0400

    mensagem

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 import express from "express";
 const app = express();
 const port = 3000;
+app.use(express.json());
+
+const usuarios = [];
 
 app.get("/", (req, res) => {
   res.send("Você está na página principal");
 });
-app.post("/", (req, res) => {
-  res.send("Você está na página principal via POST");
+
+app.get("/usuario", (req, res) => {
+  res.status(200).json(usuarios);
+});
+
+app.post("/usuario", (req, res) => {
+  const { nome, idade } = req.body;
+  console.log(req.body);
+  usuarios.push({ nome, idade });
+  console.log(usuarios);
+
+  res.status(201).send("Usuário criado com sucesso");
 });
 
 app.listen(3000, () => {
